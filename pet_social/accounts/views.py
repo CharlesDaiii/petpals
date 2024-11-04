7548e39586e03ccc264b41a7df0a9774fcbc2ee3
ziyingq <ziyingq@andrew.cmu.edu>
2024-11-04 15:13:00 -0500

    first register and login

---
+++ pet_social/accounts/views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import RegisterForm
+
+class RegisterView(View):
+    def get(self, request):
+        form = RegisterForm()
+        return render(request, 'accounts/register.html', {'form': form})
+
+    def post(self, request):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        return render(request, 'accounts/register.html', {'form': form})
+
+class LoginView(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, 'accounts/login.html', {'form': form})
+
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+        return render(request, 'accounts/login.html', {'form': form})
