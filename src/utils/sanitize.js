80477cd0cfcd4443413429bb9eeddef352755c87
miaoyyeah <miaoyany@andrew.cmu.edu>
2024-12-01 03:17:48 -0500

    add sanitize

---
+++ src/utils/sanitize.js
@@ -0,0 +1,7 @@
+// sanitize.js
+export function sanitize(s) {
+    return s.replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;');
+}
